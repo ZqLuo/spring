8fdd2c3e7c0b3fa735babc2b209b889bc9671af0
ZqLuo <540464544@qq.com>
2017-04-13 22:30:55 +0800

    注解事务控制

--- spring-jpa/src/main/java/com/example/SpringJpaApplication.java
+++ spring-jpa/src/main/java/com/example/SpringJpaApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
+//开启事务控制
 @EnableTransactionManagement
 public class SpringJpaApplication {
 
